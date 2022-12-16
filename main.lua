701108bda6a322299f5820e3fbcfb972d868e453
Butros55 <geret.w@web.de>
2022-12-16 19:38:52 +0100

    Fixed players Screen resolution and added keyswasPressed

--- main.lua
+++ main.lua
@@ -21,11 +21,6 @@ require 'src/states/PlayState'
 require 'src/states/Constants'
 
 
--- initialze all States
-gStateMachine = StateMachine{
-    ['play'] = function() return PlayState() end
-}
-
 -- Loads all content we need for this Game
 function love.load()
     -- Deafult Filter for better 2d pixel looks
@@ -37,11 +32,58 @@ function love.load()
     -- Sets Windowes Title to some Name
     love.window.setTitle('Test Project')
 
+    love.window.setMode(0, 0)
+    screen_height = love.graphics.getHeight()
+    screen_width = love.graphics.getWidth()
+
     -- Setting Screen and resolution to Players Screen
-    push:setupScreen(VIRTUAL_WINDOW_WIDTH, VIRTUAL_WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WINDOW_WIDTH, VIRTUAL_WINDOW_HEIGHT, screen_width, screen_height, {
         resizable = true,
         fullscreen = true,
         vsync = true
     })
 
+    -- initialze all States
+    -- (play) the gameplay itself
+    gStateMachine = StateMachine{
+        ['play'] = function() return PlayState() end
+    }
+    gStateMachine:change('play')
+
+    -- empty table to track which keys have been pressed
+    love.keyboard.keysPressed = {}
+
+end
+
+-- scale resolutuon if screen gets resized
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+
+function love.update(dt)
+    -- Updates currents StateMachine State
+    gStateMachine:update(dt)
+
+    -- reset keysPressed table for new input
+    love.keyboard.keysPressed = {}
+end
+
+
+-- Get users input and give it to the keysPressed table
+function love.keysPressed(key)
+    -- adding to keysPressed table the user input
+    love.keyboard.keysPressed[key] = true
+end
+
+
+-- Custom function to dectect if key was pressed
+-- returns true if that key was in table
+-- gets called if user gives input via keyboard and returns it just onc
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
 end
--- src/states/Constants.lua
+++ src/states/Constants.lua
@@ -8,11 +8,5 @@
     Global constans for this Game
 ]]
 
-screen_height = love.graphics.getHeight()
-screen_width = love.graphics.getWidth()
-
-WINDOW_HEIGHT = screen_height
-WINDOW_WIDTH = screen_width
-
-VIRTUAL_WINDOW_HEIGHT = screen_height
-VIRTUAL_WINDOW_WIDTH = screen_width
+VIRTUAL_WINDOW_HEIGHT = 1920
+VIRTUAL_WINDOW_WIDTH = 1080
